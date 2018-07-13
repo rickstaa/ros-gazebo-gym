71ab8cafa9696c1d040203134ebdb68722e97b3e
RDaneelOlivaw <duckfrost@gmail.com>
2018-07-13 16:37:39 +0000

    Added Barebones of SumitXL Env

--- openai_ros/src/openai_ros/__init__.py
+++ openai_ros/src/openai_ros/__init__.py
@@ -9,4 +9,7 @@ from openai_ros.turtlebot2.turtlebot2_maze import TurtleBot2MazeEnv
 # Turtlebot3
 from openai_ros.turtlebot3_env import TurtleBot3Env
 from openai_ros.turtlebot3.turtlebot3_world import TurtleBot3WorldEnv
+# SumitXL
+from openai_ros.sumitxl_env import SumitXlEnv
+from openai_ros.sumit_xl.sumit_xl_room import TurtleBot3WorldEnv
 
