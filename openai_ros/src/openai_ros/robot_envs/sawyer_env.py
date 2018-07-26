a51ac51279af5025a0e0116844081572781c37dc
RDaneelOlivaw <duckfrost@gmail.com>
2018-07-26 11:41:48 +0000

    Adding movement system for Sawyer

--- openai_ros/src/openai_ros/robot_envs/sawyer_env.py
+++ openai_ros/src/openai_ros/robot_envs/sawyer_env.py
@@ -2,8 +2,9 @@ import numpy
 import rospy
 import time
 from openai_ros import robot_gazebo_env
-from nav_msgs.msg import Odometry
-from robotx_gazebo.msg import UsvDrive
+import intera_interface
+import intera_external_devices
+from intera_interface import CHECK_VERSION
 
 
 
@@ -28,7 +29,7 @@ class SawyerEnv(robot_gazebo_env.RobotGazeboEnv):
         * /wamv/odom: Odometry of the Base of Wamv
         
         Actuators Topic List: 
-        * /cmd_drive: You publish the speed of the left and right propellers.
+        * As actuator we will use a class to interface with the movements through commands.
         
         Args:
         """
@@ -56,19 +57,10 @@ class SawyerEnv(robot_gazebo_env.RobotGazeboEnv):
         self.gazebo.unpauseSim()
         #self.controllers_object.reset_controllers()
         
+        # TODO: Fill it with the sensors
         self._check_all_systems_ready()
 
-
-        # We Start all the ROS related Subscribers and publishers
-        rospy.Subscriber("/wamv/odom", Odometry, self._odom_callback)
-        
-
-        self.publishers_array = []
-        self._cmd_drive_pub = rospy.Publisher('/cmd_drive', UsvDrive, queue_size=1)
-        
-        self.publishers_array.append(self._cmd_drive_pub)
-
-        self._check_all_publishers_ready()
+        self._check_movement_system_ready()
 
         self.gazebo.pauseSim()
         
@@ -94,53 +86,112 @@ class SawyerEnv(robot_gazebo_env.RobotGazeboEnv):
 
     def _check_all_sensors_ready(self):
         rospy.logdebug("START ALL SENSORS READY")
-        self._check_odom_ready()
+        # TODO: Here go the sensors like cameras and joint states
         rospy.logdebug("ALL SENSORS READY")
 
         
-    def _check_odom_ready(self):
-        self.odom = None
-        rospy.logdebug("Waiting for /wamv/odom to be READY...")
-        while self.odom is None and not rospy.is_shutdown():
-            try:
-                self.odom = rospy.wait_for_message("/wamv/odom", Odometry, timeout=1.0)
-                rospy.logdebug("Current /wamv/odom READY=>")
-
-            except:
-                rospy.logerr("Current /wamv/odom not ready yet, retrying for getting odom")
-        return self.odom
-        
-        
-    
-    def _odom_callback(self, data):
-        self.odom = data
-    
-    
-    def _check_all_publishers_ready(self):
+    def _check_movement_system_ready(self):
         """
-        Checks that all the publishers are working
+        Setup of the movement system.
         :return:
         """
-        rospy.logdebug("START ALL SENSORS READY")
-        for publisher_object in self.publishers_array:
-            self._check_pub_connection(publisher_object)
-        rospy.logdebug("ALL SENSORS READY")
-
-    def _check_pub_connection(self, publisher_object):
-
-        rate = rospy.Rate(10)  # 10hz
-        while publisher_object.get_num_connections() == 0 and not rospy.is_shutdown():
-            rospy.logdebug("No susbribers to publisher_object yet so we wait and try again")
-            try:
-                rate.sleep()
-            except rospy.ROSInterruptException:
-                # This is to avoid error when world is rested, time when backwards.
-                pass
-        rospy.logdebug("publisher_object Publisher Connected")
-
-        rospy.logdebug("All Publishers READY")
+        rp = intera_interface.RobotParams()
+        valid_limbs = rp.get_limb_names()
+        if not valid_limbs:
+            rp.log_message(("Cannot detect any limb parameters on this robot. "
+                            "Exiting."), "ERROR")
+            return
         
+        rospy.loginfo("Valid Sawyer Limbs==>"+str(valid_limbs))
+        
+        print("Getting robot state... ")
+        rs = intera_interface.RobotEnable(CHECK_VERSION)
+        init_state = rs.state().enabled
+        
+        rospy.loginfo("Enabling robot...")
+        rs.enable()
+        self.map_actions_to_movement()
+        
+        
+    def map_actions_to_movement(self, side="right"):
+        
+        limb = intera_interface.Limb(side)
+
+        try:
+            gripper = intera_interface.Gripper(side + '_gripper')
+        except:
+            has_gripper = False
+            rospy.loginfo("The electric gripper is not detected on the robot.")
+        else:
+            has_gripper = True
     
+        joints = limb.joint_names()
+    
+        bindings = {
+            '1': (self.set_j, [limb, joints[0], 0.1], joints[0]+" increase"),
+            'q': (self.set_j, [limb, joints[0], -0.1], joints[0]+" decrease"),
+            '2': (self.set_j, [limb, joints[1], 0.1], joints[1]+" increase"),
+            'w': (self.set_j, [limb, joints[1], -0.1], joints[1]+" decrease"),
+            '3': (self.set_j, [limb, joints[2], 0.1], joints[2]+" increase"),
+            'e': (self.set_j, [limb, joints[2], -0.1], joints[2]+" decrease"),
+            '4': (self.set_j, [limb, joints[3], 0.1], joints[3]+" increase"),
+            'r': (self.set_j, [limb, joints[3], -0.1], joints[3]+" decrease"),
+            '5': (self.set_j, [limb, joints[4], 0.1], joints[4]+" increase"),
+            't': (self.set_j, [limb, joints[4], -0.1], joints[4]+" decrease"),
+            '6': (self.set_j, [limb, joints[5], 0.1], joints[5]+" increase"),
+            'y': (self.set_j, [limb, joints[5], -0.1], joints[5]+" decrease"),
+            '7': (self.set_j, [limb, joints[6], 0.1], joints[6]+" increase"),
+            'u': (self.set_j, [limb, joints[6], -0.1], joints[6]+" decrease")
+         }
+        if has_gripper:
+            bindings.update({
+            '8': (self.set_g, "close", side+" gripper close"),
+            'i': (self.set_g, "open", side+" gripper open"),
+            '9': (self.set_g, "calibrate", side+" gripper calibrate")
+            })
+        done = False
+        print("Controlling joints. Press ? for help, Esc to quit.")
+        while not done and not rospy.is_shutdown():
+            c = intera_external_devices.getch()
+            if c:
+                #catch Esc or ctrl-c
+                if c in ['\x1b', '\x03']:
+                    done = True
+                    rospy.signal_shutdown("Example finished.")
+                elif c in bindings:
+                    cmd = bindings[c]
+                    if c == '8' or c == 'i' or c == '9':
+                        cmd[0](cmd[1])
+                        print("command: %s" % (cmd[2],))
+                    else:
+                        #expand binding to something like "self.set_j(right, 'j0', 0.1)"
+                        cmd[0](*cmd[1])
+                        print("command: %s" % (cmd[2],))
+                else:
+                    print("key bindings: ")
+                    print("  Esc: Quit")
+                    print("  ?: Help")
+                    for key, val in sorted(bindings.items(),
+                                           key=lambda x: x[1][2]):
+                        print("  %s: %s" % (key, val[2]))
+
+                        
+    def set_j(self,limb, joint_name, delta):
+        current_position = limb.joint_angle(joint_name)
+        joint_command = {joint_name: current_position + delta}
+        limb.set_joint_positions(joint_command)
+
+    def set_g(self,action):
+        if has_gripper:
+            if action == "close":
+                gripper.close()
+            elif action == "open":
+                gripper.open()
+            elif action == "calibrate":
+                gripper.calibrate()
+        
+        
+
     # Methods that the TrainingEnvironment will need to define here as virtual
     # because they will be used in RobotGazeboEnv GrandParentClass and defined in the
     # TrainingEnvironment.
@@ -176,29 +227,4 @@ class SawyerEnv(robot_gazebo_env.RobotGazeboEnv):
         
     # Methods that the TrainingEnvironment will need.
     # ----------------------------
-    def set_propellers_speed(self, right_propeller_speed, left_propeller_speed, time_sleep=1.0):
-        """
-        It will set the speed of each of the two proppelers of wamv.
-        """
-        i = 0
-        for publisher_object in self.publishers_array:
-          usv_drive_obj = UsvDrive()
-          usv_drive_obj.right = right_propeller_speed
-          usv_drive_obj.left = left_propeller_speed
-          
-          rospy.logdebug("usv_drive_obj>>"+str(usv_drive_obj))
-          publisher_object.publish(usv_drive_obj)
-          i += 1
-        self.wait_time_for_execute_movement(time_sleep)
     
-    def wait_time_for_execute_movement(self, time_sleep):
-        """
-        Because this Wamv position is global, we really dont have
-        a way to know if its moving in the direction desired, because it would need
-        to evaluate the diference in position and speed on the local reference.
-        """
-        time.sleep(time_sleep)
-    
-    def get_odom(self):
-        return self.odom
-
