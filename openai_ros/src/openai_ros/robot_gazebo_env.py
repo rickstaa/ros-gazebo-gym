95c9e3302a45978dca91dddf2b3bfc29707cfd5c
RDaneelOlivaw <duckfrost@gmail.com>
2018-07-31 14:28:46 +0000

    Added rewards messages to package

--- openai_ros/src/openai_ros/robot_gazebo_env.py
+++ openai_ros/src/openai_ros/robot_gazebo_env.py
@@ -4,7 +4,7 @@ from gym.utils import seeding
 from .gazebo_connection import GazeboConnection
 from .controllers_connection import ControllersConnection
 #https://bitbucket.org/theconstructcore/theconstruct_msgs/src/master/msg/RLExperimentInfo.msg
-from theconstruct_msgs.msg import RLExperimentInfo
+from openai_ros.msg import RLExperimentInfo
 
 # https://github.com/openai/gym/blob/master/gym/core.py
 class RobotGazeboEnv(gym.Env):
