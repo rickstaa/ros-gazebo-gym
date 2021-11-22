c1f89512fcc326b289867f06cd06887a6301c951
rickstaa <rick.staa@outlook.com>
2021-11-22 12:20:05 +0100

    style(turtlebot3): clean up code

---
+++ openai_ros/src/openai_ros/robot_envs/__init__.py
@@ -0,0 +1,4 @@
+"""Contains all openai_ros robot environments.
+"""
+
+from openai_ros.robot_envs.turtlebot3_env import TurtleBot3Env
--- openai_ros/src/openai_ros/robot_envs/turtlebot3_env.py
+++ openai_ros/src/openai_ros/robot_envs/turtlebot3_env.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Robot environment for the Turtlebot3. This is the new version of the classic
-Turtlebot2 created by `ROBOTIS <www.robotis.us>`_.
+Turtlebot2 created by `ROBOTIS <https://www.robotis.us>`__.
 """
 import time
 
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from openai_ros import robot_gazebo_env
 from openai_ros.core import ROSLauncher
+from openai_ros import robot_gazebo_env
 from sensor_msgs.msg import Imu, LaserScan
 
 
@@ -87,114 +87,11 @@ class TurtleBot3Env(robot_gazebo_env.RobotGazeboEnv):
 
         # Pause the simulation
         self.gazebo.pause_sim()
-        rospy.logdebug("TurtleBot3Env environment initialized.")
+        rospy.logdebug("TurtleBot3Env robot environment initialized.")
 
-    #############################################
-    # Robot environment internal methods ########
-    #############################################
-    def _check_all_sensors_ready(self):
-        """Check if all sensors are ready."""
-        rospy.logdebug("START ALL SENSORS READY")
-        self._check_odom_ready()
-        self._check_imu_ready()
-        self._check_laser_scan_ready()
-        rospy.logdebug("ALL SENSORS READY")
-
-    def _check_odom_ready(self):
-        """Checks if the odometry sensor is ready and wait till this it is.
-
-        Returns:
-            :obj:`nav_msgs.msg._Odometry.Odometry`: Odometry message.
-        """
-        self.odom = None
-        rospy.logdebug("Waiting for /odom to be READY...")
-        while self.odom is None and not rospy.is_shutdown():
-            try:
-                self.odom = rospy.wait_for_message("/odom", Odometry, timeout=5.0)
-                rospy.logdebug("Current /odom READY=>")
-            except Exception:
-                rospy.logerr("Current /odom not ready yet, retrying for getting odom")
-        return self.odom
-
-    def _check_imu_ready(self):
-        """Checks if the Inertial Measurement Unit is ready and wait till this it is.
-
-        Returns:
-            :obj:`sensor_msgs.msg._Imu.Imu`: IMU message.
-        """
-        self.imu = None
-        rospy.logdebug("Waiting for /imu to be READY...")
-        while self.imu is None and not rospy.is_shutdown():
-            try:
-                self.imu = rospy.wait_for_message("/imu", Imu, timeout=5.0)
-                rospy.logdebug("Current /imu READY=>")
-            except Exception:
-                rospy.logerr("Current /imu not ready yet, retrying for getting imu")
-        return self.imu
-
-    def _check_laser_scan_ready(self):
-        """Checks if the laser scanner is ready and wait till this it is.
-
-        Returns:
-            :obj:`sensor_msgs.msg._LaserScan.LaserScan`: Laser scanner message.
-        """
-        self.laser_scan = None
-        rospy.logdebug("Waiting for /scan to be READY...")
-        while self.laser_scan is None and not rospy.is_shutdown():
-            try:
-                self.laser_scan = rospy.wait_for_message(
-                    "/scan", LaserScan, timeout=1.0
-                )
-                rospy.logdebug("Current /scan READY=>")
-            except Exception:
-                rospy.logerr(
-                    "Current /scan not ready yet, retrying for getting laser_scan"
-                )
-        return self.laser_scan
-
-    def _odom_callback(self, data):
-        """Odometry subscriber callback function.
-
-        Args:
-            data (:obj:`nav_msgs.msg._Odometry.Odometry`): The data that is returned by
-                the subscriber.
-        """
-        self.odom = data
-
-    def _imu_callback(self, data):
-        """IMU subscriber callback function.
-
-        Args:
-            data (:obj:`sensor_msgs.msg._Imu.Imu`): The data that is returned by
-                the subscriber.
-        """
-        self.imu = data
-
-    def _laser_scan_callback(self, data):
-        """Laser scanner subscriber callback function.
-
-        Args:
-            data (:obj:`sensor_msgs.msg._LaserScan.LaserScan`): The data that is
-                returned by the subscriber.
-        """
-        self.laser_scan = data
-
-    def _check_publishers_connection(self):
-        """Checks that all the publishers are working"""
-        rate = rospy.Rate(10)  # 10hz
-        while self._cmd_vel_pub.get_num_connections() == 0 and not rospy.is_shutdown():
-            rospy.logdebug("No susbribers to _cmd_vel_pub yet so we wait and try again")
-            try:
-                rate.sleep()
-            except rospy.ROSInterruptException:
-                # This is to avoid error when world is rested, time when backwards.
-                pass
-        rospy.logdebug("_cmd_vel_pub Publisher Connected")
-        rospy.logdebug("All Publishers READY")
-
-    #############################################
-    # Robot env main methods ####################
-    #############################################
+    ################################################
+    # Robot env main methods #######################
+    ################################################
     # NOTE: Contains methods that the TrainingEnvironment will need.
     def move_base(self, linear_speed, angular_speed, epsilon=0.05, update_rate=10):
         """Move the robot base iwth a given linear and angular speed.
@@ -320,10 +217,114 @@ class TurtleBot3Env(robot_gazebo_env.RobotGazeboEnv):
         """
         return self.laser_scan
 
-    #############################################
-    # Overload Gazebo env virtual methods #######
-    #############################################
-    # NOTE: Methods needed by the Robot or gazebo environments
+    ################################################
+    # Panda Robot env helper methods ###############
+    ################################################
+
+    def _check_all_sensors_ready(self):
+        """Check if all sensors are ready."""
+        rospy.logdebug("START ALL SENSORS READY")
+        self._check_odom_ready()
+        self._check_imu_ready()
+        self._check_laser_scan_ready()
+        rospy.logdebug("ALL SENSORS READY")
+
+    def _check_odom_ready(self):
+        """Checks if the odometry sensor is ready and wait till this it is.
+
+        Returns:
+            :obj:`nav_msgs.msg._Odometry.Odometry`: Odometry message.
+        """
+        self.odom = None
+        rospy.logdebug("Waiting for /odom to be READY...")
+        while self.odom is None and not rospy.is_shutdown():
+            try:
+                self.odom = rospy.wait_for_message("/odom", Odometry, timeout=5.0)
+                rospy.logdebug("Current /odom READY=>")
+            except Exception:
+                rospy.logerr("Current /odom not ready yet, retrying for getting odom")
+        return self.odom
+
+    def _check_imu_ready(self):
+        """Checks if the Inertial Measurement Unit is ready and wait till this it is.
+
+        Returns:
+            :obj:`sensor_msgs.msg._Imu.Imu`: IMU message.
+        """
+        self.imu = None
+        rospy.logdebug("Waiting for /imu to be READY...")
+        while self.imu is None and not rospy.is_shutdown():
+            try:
+                self.imu = rospy.wait_for_message("/imu", Imu, timeout=5.0)
+                rospy.logdebug("Current /imu READY=>")
+            except Exception:
+                rospy.logerr("Current /imu not ready yet, retrying for getting imu")
+        return self.imu
+
+    def _check_laser_scan_ready(self):
+        """Checks if the laser scanner is ready and wait till this it is.
+
+        Returns:
+            :obj:`sensor_msgs.msg._LaserScan.LaserScan`: Laser scanner message.
+        """
+        self.laser_scan = None
+        rospy.logdebug("Waiting for /scan to be READY...")
+        while self.laser_scan is None and not rospy.is_shutdown():
+            try:
+                self.laser_scan = rospy.wait_for_message(
+                    "/scan", LaserScan, timeout=1.0
+                )
+                rospy.logdebug("Current /scan READY=>")
+            except Exception:
+                rospy.logerr(
+                    "Current /scan not ready yet, retrying for getting laser_scan"
+                )
+        return self.laser_scan
+
+    def _odom_callback(self, data):
+        """Odometry subscriber callback function.
+
+        Args:
+            data (:obj:`nav_msgs.msg._Odometry.Odometry`): The data that is returned by
+                the subscriber.
+        """
+        self.odom = data
+
+    def _imu_callback(self, data):
+        """IMU subscriber callback function.
+
+        Args:
+            data (:obj:`sensor_msgs.msg._Imu.Imu`): The data that is returned by
+                the subscriber.
+        """
+        self.imu = data
+
+    def _laser_scan_callback(self, data):
+        """Laser scanner subscriber callback function.
+
+        Args:
+            data (:obj:`sensor_msgs.msg._LaserScan.LaserScan`): The data that is
+                returned by the subscriber.
+        """
+        self.laser_scan = data
+
+    def _check_publishers_connection(self):
+        """Checks that all the publishers are working"""
+        rate = rospy.Rate(10)  # 10hz
+        while self._cmd_vel_pub.get_num_connections() == 0 and not rospy.is_shutdown():
+            rospy.logdebug("No susbribers to _cmd_vel_pub yet so we wait and try again")
+            try:
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                # This is to avoid error when world is rested, time when backwards.
+                pass
+        rospy.logdebug("_cmd_vel_pub Publisher Connected")
+        rospy.logdebug("All Publishers READY")
+
+    ################################################
+    # Overload Gazebo env virtual methods ##########
+    ################################################
+    # NOTE: Methods needed by the gazebo environment
     def _check_all_systems_ready(self):
         """Checks that all the sensors, publishers and other simulation systems are
         operational.
