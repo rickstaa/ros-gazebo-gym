c5d7bcbdcf14dbcfc86f48ddf5cd8ecb8c97feef
rickstaa <rick.staa@outlook.com>
2021-11-22 10:07:03 +0100

    refractor: move 'start_openai_ros_env' into own file

--- openai_ros/src/openai_ros/__init__.py
+++ openai_ros/src/openai_ros/__init__.py
@@ -1,2 +1,2 @@
-from openai_ros.core.core import start_openai_ros_env
+from openai_ros.core.start_openai_ros_env import start_openai_ros_env
 from openai_ros.core.ros_launcher import ROSLauncher
--- openai_ros/src/openai_ros/core/__init__.py
+++ openai_ros/src/openai_ros/core/__init__.py
@@ -1,6 +1,7 @@
-"""Contains functions and classes that can be used with all the openai_ros gym
-environments.
+"""Contains functions and classes that are used to setup the openai_ros gym
+environments. This contains functions for download the required dependencies, starting
+ROS launch files, connecting to the gazebo simulation, ect.
 """
-from openai_ros.core.core import start_openai_ros_env
+from openai_ros.core.start_openai_ros_env import start_openai_ros_env
 from openai_ros.core.gazebo_connection import GazeboConnection
 from openai_ros.core.ros_launcher import ROSLauncher
